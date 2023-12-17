79801c00557f42094271d2992a5f685ab2f579e3
Shravan <shravansinghchaudhary@gmail.com>
2023-12-17 15:20:02 +0530

    feat(client-admin): extend common tailwind css configuration in client-admin app

--- apps/client-admin/app/page.tsx
+++ apps/client-admin/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page(): JSX.Element {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1 className="font-bold text-5xl text-red-400">Hello world</h1>
     </div>
-  )
+  );
 }
