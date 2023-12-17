eff77d11d1854c2aca74d2352f7964448a740622
Shravan <shravansinghchaudhary@gmail.com>
2023-12-17 18:28:52 +0530

    fix: fix linting issues

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,10 +1,13 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/nextjs.js"],
+  extends: ["@repo/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.lint.json",
   },
-  ignorePatterns: ["tailwind.config.js"],
+  ignorePatterns: ["tailwind.config.js", "postcss.config.js"],
+  rules: {
+    "no-html-link-for-pages": "off",
+  },
 };
